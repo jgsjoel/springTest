771ff5b58800c9dc367481c77af8abba3c029d1e
joel <jgsjoelsmith@gmail.com>
2024-06-05 15:04:03 +0530

    removed hibernate and db

--- src/main/java/com/mcom/mcomapi/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mcom.mcomapi.repositories;
-
-import com.mcom.mcomapi.entities.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-}
